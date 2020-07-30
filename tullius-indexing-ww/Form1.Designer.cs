fe4ea1b2b59083f25f455ac109ad00550c473e71
violet-dev <violet.dev.master@gmail.com>
2020-07-30 22:23:28 +0900

    add blocked minor gallery archive tools

---
+++ tullius-indexing-ww/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace tullius_indexing_ww
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 75);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 54);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "시작";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(136, 106);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(142, 23);
+            this.textBox4.TabIndex = 18;
+            this.textBox4.Text = "2000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 15);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "인덱싱 끝 페이지: ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(136, 75);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(142, 23);
+            this.textBox3.TabIndex = 16;
+            this.textBox3.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 15);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "인덱싱 시작 페이지: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "비밀번호: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "아이디: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 44);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 23);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 23);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 54);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 162);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(418, 316);
+            this.richTextBox1.TabIndex = 22;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(75, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 15);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "상태: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 486);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "몬무스갤 인덱싱 툴 / 2020.07.30";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ tullius-indexing-ww/Form1.cs
@@ -0,0 +1,274 @@
+﻿using Newtonsoft.Json;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tulius_archive_ww;
+
+namespace tullius_indexing_ww
+{
+    public partial class Form1 : Form
+    {
+        protected ChromeDriverService _driverService = null;
+        protected ChromeOptions _options = null;
+        protected ChromeDriver _driver = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _driverService = ChromeDriverService.CreateDefaultService();
+            _driverService.HideCommandPromptWindow = true;
+
+            _options = new ChromeOptions();
+            _options.AddArgument("disable-gpu");
+        }
+
+        static public string COOKIES = "";
+        static public string SESS = "";
+
+        private void richTextBox_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+
+        void append(string ww) =>
+            Extends.Post(this, () => richTextBox1.AppendText(ww + "\r\n"));
+        void status(string ww) => Extends.Post(this, () => label6.Text = ww);
+
+        private void login()
+        {
+            append("로그인 요청 시작 id=" + textBox1.Text + ", pwd=" + textBox2.Text);
+            _driver = new ChromeDriver(_driverService, _options);
+            _driver.Navigate().GoToUrl("https://www.dcinside.com/");
+
+            Thread.Sleep(500);
+
+            var element = _driver.FindElementByXPath("//input[@id='user_id']");
+            element.SendKeys(textBox1.Text);
+            append("Append userid to //input[@id='user_id']");
+
+            element = _driver.FindElementByXPath("//input[@id='pw']");
+            element.SendKeys(textBox2.Text);
+            append("Append userpwd to //input[@id='user_id']");
+
+            element = _driver.FindElementByXPath("//button[@id='login_ok']");
+            element.Click();
+            append("Remote login button click");
+
+            Thread.Sleep(500);
+
+            try
+            {
+                var x = _driver.Manage().Cookies;
+                COOKIES = string.Join("; ", x.AllCookies.Select(y => y.Name + "=" + y.Value));
+                append("쿠키: " + COOKIES);
+                SESS = x.AllCookies.First(y => y.Name == "PHPSESSID").Value;
+                append("세션쿠키: " + SESS);
+                if (SESS != "")
+                    append("로그인 성공");
+                _driver.Close();
+                _driver.Dispose();
+                _driverService.Dispose();
+            }
+            catch (Exception e)
+            {
+                append("예외: " + e.Message);
+                if (SESS != "")
+                    append("로그인 실패");
+                _driver.Close();
+                _driver.Dispose();
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+
+            await Task.Run(() => login());
+            if (SESS != "")
+            {
+                button1.Enabled = false;
+                label6.Text = "로그인 성공. 시작 버튼을 누르세요";
+            }
+            else
+            {
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+
+                label6.Text = "로그인 실패. 다시 시도해주세요";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            Task.Run(() => start());
+            //var wc = NetCommon.GetDefaultClient();
+            //wc.Headers.Add("X-Requested-With", "XMLHttpRequest");
+            //wc.QueryString.Add("under_name", un);
+            //var subhtml = Encoding.UTF8.GetString(wc.UploadValues("https://gall.dcinside.com/ajax/minor_ajax/get_under_gall", "POST", wc.QueryString));
+
+            //var task = NetTask.MakeDefault("https://gall.dcinside.com/mgallery/board/lists/?id=github&page=1");
+            //task.Cookie = "PHPSESSID=" + SESS;
+            //var xx = NetTools.DownloadString(task);
+        }
+
+        private void start()
+        {
+            int ps;
+            if (!int.TryParse(textBox3.Text, out ps))
+            {
+                append("숫자만 입력해주세요.");
+                return;
+            }
+            int pe;
+            if (!int.TryParse(textBox4.Text, out pe))
+            {
+                append("숫자만 입력해주세요.");
+                return;
+            }
+
+            var id = "monmusu";
+
+            var starts = ps;
+
+            status("진행중...[0/" + (pe - ps + 1).ToString("#,#") + "]");
+            bool real_cookie_receive = false;
+
+            var articles = new List<DCInsidePageArticle>();
+
+            try
+            {
+                for (; ps <= pe; ps++)
+                {
+                    string url;
+
+                    if (true)
+                        url = $"https://gall.dcinside.com/mgallery/board/lists/?id={id}&page={ps}";
+                    else
+                        url = $"https://gall.dcinside.com/board/lists/?id={id}&page={ps}";
+
+                    Logger.Instance.Push("Downloading String... = " + url);
+                    var task = NetTask.MakeDefault(url);
+                    task.Cookie = COOKIES;
+                    bool y = real_cookie_receive;
+                    if (real_cookie_receive == false)
+                    {
+                        task.HeaderReceive = (ef) =>
+                        {
+                            append("헤더받음: " + ef);
+                            var xx = ef.Split('\n').First(x => x.Contains("Set-Cookie")).Replace("Set-Cookie: ", "").Trim();
+                            COOKIES = "PHPSESSID=" + SESS;
+                            COOKIES += "; PHPSESSKEY=" + xx.Split(new[] { "PHPSESSKEY=" }, StringSplitOptions.None)[1].Split(';')[0].Trim();
+                            COOKIES += $"; block_alert_{id}=1";
+                            real_cookie_receive = true;
+                            append("쿠키 변경됨: " + COOKIES);
+                        };
+                    }
+                    var html = NetTools.DownloadString(task);
+                    if (y == false)
+                    {
+                        append("PS1");
+                        ps--;
+                        continue;
+                    }
+                    Logger.Instance.Push("Downloaded String = " + url);
+                    if (string.IsNullOrEmpty(html))
+                    {
+                        append("실패: " + url);
+                        Logger.Instance.Push("Fail: " + url);
+                        goto NEXT;
+                    }
+                    if (html.Length < 1000 && html.Contains("해당 마이너 갤러리는 운영원칙 위반으로 접근이 제한되었습니다."))
+                    {
+                        append("실패: 접근 거부, 접근 가능한 아이디로 재시도하시기 바랍니다.");
+                        break;
+                    }
+                    if (html.Contains("해당 마이너 갤러리는 운영원칙 위반(사유: 누드패치, 성행위 패치, 음란성 게시물 공지 등록 및 정리 안됨) 으로 접근이 제한되었습니다."))
+                    {
+                        //goto FUCK;
+                        return;
+                    }
+
+                    DCInsideGallery gall;
+
+                    if (true)
+                        gall = DCInsideUtils.ParseMinorGallery(html);
+                    else
+                        gall = DCInsideUtils.ParseGallery(html);
+
+                    if (true && (gall.articles == null || gall.articles.Length == 0))
+                        gall = DCInsideUtils.ParseGallery(html);
+
+                    if (gall.articles.Length == 0)
+                        break;
+
+                    articles.AddRange(gall.articles);
+
+                    Logger.Instance.Push("Parse: " + url);
+                    // 해당 마이너 갤러리는 운영원칙 위반으로 접근이 제한되었습니다.\n마이너 갤러리 메인으로 돌아갑니다.
+
+               NEXT:
+                    var ss = TimeSpan.FromMilliseconds(720 * (pe - ps));
+                    var yy = "";
+                    if (ss.Days > 0)
+                        yy += ss.Days.ToString() + "일 ";
+                    if (ss.Days > 0 || ss.Hours > 0)
+                        yy += ss.Hours.ToString() + "시간 ";
+                    if (ss.Days > 0 || ss.Hours > 0 || ss.Minutes > 0)
+                        yy += ss.Minutes.ToString() + "분 ";
+                    if (ss.Days > 0 || ss.Hours > 0 || ss.Minutes > 0 || ss.Seconds > 0)
+                        yy += ss.Seconds.ToString() + "초 남음";
+
+                    status("진행중...[" + (ps - starts + 1).ToString() + "/" + (pe - starts + 1).ToString("#,#") + "] 남은시간: " + yy);
+                    Logger.Instance.Push("next: " + url + $" || {ps}/{pe}/{starts}");
+                    Thread.Sleep(700);
+                }
+
+                DCGalleryAnalyzer.Instance.Articles.AddRange(articles);
+                DCGalleryAnalyzer.Instance.Save();
+
+                status("완료");
+            }
+            catch (Exception e)
+            {
+                append("실패: " + e.Message + "\r\n" + e.StackTrace);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //DCGalleryAnalyzer.Instance.Open("툴리우스갤 데이터.txt");
+        }
+    }
+
+    public static class Extends
+    {
+        public static T Send<T>(this Control control, Func<T> func)
+            => control.InvokeRequired ? (T)control.Invoke(func) : func();
+
+        public static void Post(this Control control, Action action)
+        {
+            if (control.InvokeRequired) control.BeginInvoke(action);
+            else action();
+        }
+
+        public static int ToInt32(this string str) => Convert.ToInt32(str);
+    }
+}
